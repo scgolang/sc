281173b068a16a46cf719ef2337046f05a1ffd3a
Brian Sorahan <bsorahan@gmail.com>
2015-02-02 00:09:30 -0600

    put node in its own file

--- synthdefs/out.go
+++ synthdefs/out.go
@@ -7,35 +7,43 @@ import (
 
 var Out = NewMetaOut()
 
-type MetaOut struct {
-	index int32
-	channels []gosc.Synth
-}
+type MetaOut struct {}
 
-func (self *MetaOut) Ar(args ...interface{}) (*MetaOut, error) {
-	var index int32
-
-	channels := make([]gosc.Synth, 0)
+func (self *MetaOut) Ar(args ...interface{}) (*gosc.Node, error) {
+	root := gosc.NewNode(nil)
 
 	if len(args) < 2 {
 		return nil, fmt.Errorf("not enough arguments to Out.Ar")
 	}
+	// get the index
 	if index, ok := args[0].(int32); !ok {
 		return nil, fmt.Errorf("first argument to Out.Ar must be an int32")
+	} else {
+		root.Add(gosc.NewNode(index))
 	}
+
 	// just a single synth?
 	if channels, ok := args[1].(gosc.Synth); !ok {
 		// an array of synths?
 		if channels, ok := args[1].([]gosc.Synth); !ok {
 			return nil, fmt.Errorf("second argument to Out.Ar must be a synth or an array of synths")
+		} else {
+			if len(channels) == 0 {
+				return nil, fmt.Errorf("Out requires at least 1 synth argument")
+			}
+			root.Add(gosc.NewNode(channels))
 		}
+	} else {
+		root.Add(gosc.NewNode(channels))
 	}
-	return self, nil
+
+	return root, nil
 }
 
-func (self *MetaOut) Kr(args ...interface{}) (*MetaOut, error) {
-	return self, nil
+func (self *MetaOut) Kr(args ...interface{}) (*gosc.Node, error) {
+	return nil, nil
 }
 
-func NewMetaOut() (MetaOut, error) {
+func NewMetaOut() *MetaOut {
+	return new(MetaOut)
 }

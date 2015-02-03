7f6b1b529cd9819f40cb255b7e4cdf297ae96c26
Brian Sorahan <bsorahan@gmail.com>
2015-02-02 23:05:59 -0600

    working on Server.Boot

---
+++ server_test.go
@@ -0,0 +1,28 @@
+package gosc
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewServer(t *testing.T) {
+	s := NewServer(NetAddr{"localhost", DefaultPort})
+	if s == nil {
+		t.Fatal(fmt.Errorf("NewServer returned nil"))
+	}
+}
+
+func TestServerBoot(t *testing.T) {
+	s := NewServer(NetAddr{"localhost", DefaultPort})
+	if s == nil {
+		t.Fatal(fmt.Errorf("NewServer returned nil"))
+	}
+	err := s.Boot()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = s.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}

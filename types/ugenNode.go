d9374c40a9bcb26841b6175f67c9b7269f405e38
Brian Sorahan <bsorahan@gmail.com>
2015-02-22 20:39:56 -0600

    cleaning up some cruft

--- types/ugenNode.go
+++ types/ugenNode.go
@@ -5,11 +5,8 @@ type UgenNode interface {
 	Name() string
 	// Rate returns the rate of the ugen node
 	Rate() int8
-	// IsOutput tells the ugen node that it is being
-	// used as the input for another ugen
-	IsOutput()
-	// Outputs returns the outputs of the ugen node
-	Outputs() []Output
 	// Inputs returns the inputs of the ugen node
 	Inputs() []Input
+	// Outputs returns the outputs of the ugen node
+	Outputs() []Output
 }

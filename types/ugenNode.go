24b03a4a65905cd4e892bedb4a6c9d110c7a498f
Brian Sorahan <bsorahan@gmail.com>
2015-02-22 22:19:50 -0600

    added Mul and Add methods to UgenNode interface for creating BinaryOpUGen nodes

--- types/ugenNode.go
+++ types/ugenNode.go
@@ -5,8 +5,16 @@ type UgenNode interface {
 	Name() string
 	// Rate returns the rate of the ugen node
 	Rate() int8
+	// SpecialIndex returns the special index of the ugen node
+	SpecialIndex() int16
 	// Inputs returns the inputs of the ugen node
 	Inputs() []Input
 	// Outputs returns the outputs of the ugen node
 	Outputs() []Output
+	// Mul multiplies the output of a ugen node by a constant
+	// and returns a new ugen node representing the resulting audio stream
+	Mul(f float32) UgenNode
+	// Add adds a constant to the output of a ugen node
+	// and returns a new ugen node representing the resulting audio stream
+	Add(f float32) UgenNode
 }

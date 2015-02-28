26c2f4a943a1ad54ef737d6715b16ba4c371905b
Brian Sorahan <brian@gospacecraft.com>
2015-02-28 12:32:13 -0600

    do not start server in unit tests

--- server_test.go
+++ server_test.go
@@ -10,17 +10,4 @@ func TestServerStatus(t *testing.T) {
 	if s == nil {
 		t.Fatal(fmt.Errorf("NewServer returned nil"))
 	}
-	err := s.Start()
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = s.Status()
-	if err != nil {
-		s.Close()
-		t.Fatal(err)
-	}
-	err = s.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
 }

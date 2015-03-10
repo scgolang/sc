3bee3911bafd3982c5e1685f37321301ff0a144f
Brian Sorahan <brian@gospacecraft.com>
2015-03-10 09:22:51 -0500

    added Env.Linen and tests

--- ugens/env.go
+++ ugens/env.go
@@ -6,14 +6,21 @@ package ugens
 // 0, 0.2, 5, 4 -- and so on
 
 const (
-	specialCurveType = 5
+	CurveStep        = 0
+	CurveLinear      = 1
+	CurveExponential = 2
+	CurveSine        = 3
+	CurveWelch       = 4
+	CurveCustom      = 5
+	CurveSquared     = 6
+	CurveCubed       = 7
 )
 
 // Env is not a ugen, but rather a way to generate
 // Control arrays that get handed to EnvGen
 var Env = newEnv()
 
-type envelope interface {
+type Envelope interface {
 	// InputsArray provides EnvGen with the data it needs
 	// to get a list of inputs
 	InputsArray() []interface{}
@@ -32,7 +39,7 @@ func (self *envelopeImpl) InputsArray() []interface{} {
 	numSegments := len(self.levels)
 	arr := make([]interface{}, 4*numSegments)
 	arr[0] = self.levels[0]
-	arr[1] = float32(numSegments-1)
+	arr[1] = float32(numSegments - 1)
 	arr[2] = self.releaseNode
 	arr[3] = self.loopNode
 	for i, t := range self.times {
@@ -48,14 +55,49 @@ type env struct {
 }
 
 // Perc http://doc.sccode.org/Classes/Env.html#*perc
-func (self *env) Perc(args ...interface{}) envelope {
+func (self *env) Perc(args ...interface{}) Envelope {
 	defaults := []float32{0.01, 1, 1, -4}
 	withDefaults := applyDefaults(defaults, args...)
 	e := envelopeImpl{
 		[]interface{}{float32(0), withDefaults[2], float32(0)},
 		[]interface{}{withDefaults[0], withDefaults[1]},
-		specialCurveType,
+		CurveCustom,
 		withDefaults[3],
+		float32(-99),
+		float32(-99),
+	}
+	return &e
+}
+
+// Linen http://doc.sccode.org/Classes/Env.html#*linen
+func (self *env) Linen(args ...interface{}) Envelope {
+	defaults := []float32{0.01, 1, 1, 1, 1}
+	withDefaults := applyDefaults(defaults, args...)
+	levels := []interface{}{
+		float32(0),
+		withDefaults[3],
+		withDefaults[3], // sustain level
+		float32(0),
+	}
+	times := []interface{}{
+		withDefaults[0],
+		withDefaults[1],
+		withDefaults[2],
+	}
+	var curve int
+	switch v := withDefaults[4].(type) {
+	case int:
+		curve = v
+	case float32:
+		curve = int(v)
+	default:
+		panic("curve must be either int or float32")
+	}
+	e := envelopeImpl{
+		levels,
+		times,
+		curve,
+		float32(0),
 		float32(-99),
 		float32(-99),
 	}
--- ugens/env_test.go
+++ ugens/env_test.go
@@ -7,16 +7,46 @@ import (
 func TestPerc(t *testing.T) {
 	e := Env.Perc()
 	expect := []float32{0, 2, -99, -99, 1, 0.01, 5, -4, 0, 1, 5, -4}
+	verifyInputs(t, expect, e.InputsArray())
+}
+
+func TestLinen(t *testing.T) {
+	e := Env.Linen()
+	expect := []float32{0, 3, -99, -99, 1, 0.01, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0}
+	verifyInputs(t, expect, e.InputsArray())
+	expectWelch := []float32{0, 3, -99, -99, 1, 0.01, 4, 0, 1, 1, 4, 0, 0, 1, 4, 0}
+	f := Env.Linen(0.01, 1, 1, 1, CurveWelch)
+	verifyInputs(t, expectWelch, f.InputsArray())
+}
+
+func verifyInputs(t *testing.T, expect []float32, inputs []interface{}) {
+	t.Logf("%v\n", inputs)
 	errmsg := "expected %f for input %d but got %f"
-	arr := e.InputsArray()
-	t.Logf("arr = %v\n", arr)
-	for i, in := range arr {
-		if floatVal, isFloat := in.(float32); isFloat {
-			if floatVal != expect[i] {
-				t.Fatalf(errmsg, expect[i], i, floatVal)
-			}
-		} else {
-			t.Fatalf("input %d was not a float (%v)", i, in)
+	for i, in := range inputs {
+		var val float32
+		switch v := in.(type) {
+		case int:
+			val = float32(v)
+		case float64:
+			val = float32(v)
+		case float32:
+			val = v
+		default:
+			t.Fatalf("input %d was not a float or int (%v)", i, in)
 		}
+		if val != expect[i] {
+			t.Fatalf(errmsg, expect[i], i, val)
+		}
+		// if floatVal, isFloat := in.(float32); isFloat {
+		// 	if floatVal != expect[i] {
+		// 		t.Fatalf(errmsg, expect[i], i, floatVal)
+		// 	}
+		// } else if intVal, isInt := in.(int); isInt {
+		// 	if float32(intVal) != expect[i] {
+		// 		t.Fatalf(errmsg, expect[i], i, intVal)
+		// 	}
+		// } else {
+		// 	t.Fatalf("input %d was not a float or int (%v)", i, in)
+		// }
 	}
 }

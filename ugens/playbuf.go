180fe863f70651c409f11597a8e69b03f288c73e
Brian Sorahan <bsorahan@gmail.com>
2015-05-13 22:15:05 -0500

    added an audio file and worked on playbuf some more

--- ugens/playbuf.go
+++ ugens/playbuf.go
@@ -2,7 +2,8 @@ package ugens
 
 import . "github.com/scgolang/sc/types"
 
-// PlayBuf play back a sample from memory
+// PlayBuf plays back a sample from memory.
+// If Buf is nil a runtime panic will occur.
 type PlayBuf struct {
 	// NumChannels number of playback channels
 	NumChannels int
@@ -27,6 +28,18 @@ func (self *PlayBuf) defaults() {
 	if self.NumChannels == 0 {
 		self.NumChannels = 1
 	}
+	if self.Speed == nil {
+		self.Speed = C(1.0)
+	}
+	if self.Trigger == nil {
+		self.Trigger = C(1)
+	}
+	if self.Start == nil {
+		self.Start = C(0)
+	}
+	if self.Loop == nil {
+		self.Loop = C(0)
+	}
 }
 
 // Rate creates a new ugen at a specific rate.
@@ -34,6 +47,11 @@ func (self *PlayBuf) defaults() {
 // a runtime panic.
 func (self PlayBuf) Rate(rate int8) Input {
 	checkRate(rate)
+	if self.Buf == nil {
+		panic("PlayBuf expects Buf to not be nil")
+	}
 	(&self).defaults()
-	return UgenInput("PlayBuf", rate, 0)
+	nchans := C(float32(self.NumChannels))
+	done := C(float32(self.Done))
+	return UgenInput("PlayBuf", rate, 0, nchans, self.Speed, self.Trigger, self.Start, self.Loop, done)
 }

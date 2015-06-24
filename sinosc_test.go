59a458ed8e56ec560a7ca40fb61989be8ec9500e
Brian Sorahan <bsorahan@gmail.com>
2015-06-24 17:10:33 -0500

    moved sinosc test out of synthdef_test. added more notes about adding ugens

---
+++ sinosc_test.go
@@ -0,0 +1,16 @@
+package sc
+
+import . "github.com/scgolang/sc/types"
+
+func TestSinOsc(t *testing.T) {
+	name := "SineTone"
+	def := NewSynthdef(name, func(params *Params) Ugen {
+		bus, freq := C(0), C(440)
+		sine := SinOsc{Freq: freq}.Rate(AR)
+		return Out{bus, sine}.Rate(AR)
+	})
+	if def == nil {
+		t.Fatalf("nil synthdef")
+	}
+	compareAndWrite(t, name, def)
+}

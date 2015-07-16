67e9dd49986be475c38f2de33878a8ae8572e52d
Brian Sorahan <bsorahan@gmail.com>
2015-07-16 16:22:26 -0500

    fix client test and use multiline string in crackle test

--- crackle_test.go
+++ crackle_test.go
@@ -10,17 +10,19 @@ import (
 func TestCrackle(t *testing.T) {
 	name := "CrackleTest"
 	def := NewSynthdef(name, func(params Params) Ugen {
-		//arg bufnum = 0;
-		//var crack = Crackle.ar(Line.kr(1.0, 2.0, 3), 0.5, 0.5);
-		//Out.ar(0, crack);
-
 		bus, chaos := C(0), Line{C(1.0), C(2.0), C(3), DoNothing}.Rate(KR)
 		sig := Crackle{chaos}.Rate(AR).MulAdd(C(0.5), C(0.5))
 		return Out{bus, sig}.Rate(AR)
 
 	})
-	if def == nil {
-		t.Fatalf("nil synthdef")
+	same, err := def.Compare(`{
+		var crack = Crackle.ar(Line.kr(1.0, 2.0, 3), 0.5, 0.5);
+		Out.ar(0, crack);
+    }`)
+	if err != nil {
+		t.Fatal(err)
 	}
-	compareAndWrite(t, name, def)
+	if !same {
+		t.Fatalf("synthdef different from sclang version")
+	}
 }

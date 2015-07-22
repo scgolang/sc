f60579d0a76a0e26c86f7466972343436bf87b78
Brian Sorahan <bsorahan@gmail.com>
2015-07-22 17:08:07 -0500

    support for BrownNoise

--- ugens/brownnoise.go
+++ ugens/brownnoise.go
@@ -2,23 +2,12 @@ package ugens
 
 import . "github.com/scgolang/sc/types"
 
-// Crackle
-// A noise generator based on a chaotic function.
-type Crackle struct {
-	Chaos Input
-}
-
-func (self *Crackle) defaults() {
-	if self.Chaos == nil {
-		self.Chaos = C(1.5)
-	}
-}
+// BrownNoise generates noise whose spectrum falls off in power by 6 dB per octave
+type BrownNoise struct{}
 
 // Rate creates a new ugen at a specific rate.
-// If rate is an unsupported value this method will cause
-// a runtime panic.
-func (self Crackle) Rate(rate int8) Input {
+// If rate is an unsupported value this method will cause a runtime panic.
+func (self BrownNoise) Rate(rate int8) Input {
 	checkRate(rate)
-	(&self).defaults()
-	return UgenInput("Crackle", rate, 0, 1, self.Chaos)
+	return UgenInput("BrownNoise", rate, 0, 1)
 }

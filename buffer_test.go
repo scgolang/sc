1b32dff09b51ad18b4578ff73a57e6a29f2479ee
Brian Sorahan <bsorahan@gmail.com>
2015-07-23 21:46:53 -0500

    added ClipNoise

--- buffer_test.go
+++ buffer_test.go
@@ -3,18 +3,19 @@ package sc
 import "testing"
 
 func TestBuffer(t *testing.T) {
-	buf := newBuffer("foo")
+	c := NewClient("127.0.0.1", 57112)
+	buf := newReadBuffer("foo", c)
 	n := buf.Num()
 	if n != 0 {
 		t.Fatalf("expected 0, but got %d", n)
 	}
-	buf = newBuffer("bar")
+	buf = newReadBuffer("bar", c)
 	n = buf.Num()
 	if n != 1 {
 		t.Fatalf("expected 1, but got %d", n)
 	}
 	// should return the first buffer
-	newBuf := newBuffer("foo")
+	newBuf := newReadBuffer("foo", c)
 	n = newBuf.Num()
 	if n != 0 {
 		t.Fatalf("expected 0, but got %d", n)

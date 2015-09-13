67570d81456f74fa8cf76abaed79e2fb59820e61
Brian Sorahan <bsorahan@gmail.com>
2015-09-13 14:40:00 -0500

    implement the synth Set method

--- synth.go
+++ synth.go
@@ -4,26 +4,40 @@ import (
 	"github.com/scgolang/osc"
 )
 
+const (
+	freeSynthNodeAddress = "/n_free"
+	setSynthNodeAddress  = "/n_set"
+)
+
+// Synth encapsulates a synth node.
 type Synth struct {
 	defName string `json:"defName"`
 	id      int32  `json:"id"`
 	client  *Client
 }
 
+// Get the value of a synth control.
 func (self *Synth) Get(controlName string) (float32, error) {
 	return 0, nil
 }
 
-func (self *Synth) Set(controlName string, val float32) error {
-	return nil
+// Set the value of a synth control.
+func (self *Synth) Set(ctls map[string]float32) error {
+	set := osc.NewMessage(setSynthNodeAddress)
+	for name, value := range ctls {
+		set.Append(name, value)
+	}
+	return self.client.oscServer.SendTo(self.client.conn, set)
 }
 
+// Free a synth node.
 func (self *Synth) Free() error {
-	free := osc.NewMessage("/n_free")
+	free := osc.NewMessage(freeSynthNodeAddress)
 	free.Append(self.id)
 	return self.client.oscServer.SendTo(self.client.conn, free)
 }
 
+// newSynth creates a new synth structure.
 func newSynth(client *Client, defName string, id int32) *Synth {
 	return &Synth{
 		defName: defName,

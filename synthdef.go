a032b330e28b0cd254e8f7f21e664454cd6b796d
Brian Sorahan <bsorahan@leaf.ag>
2016-02-13 14:23:10 -0600

    fix receiver name in Synthdef methods

--- synthdef.go
+++ synthdef.go
@@ -6,12 +6,13 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
-	"github.com/awalterschulze/gographviz"
 	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
+
+	"github.com/awalterschulze/gographviz"
 )
 
 const (
@@ -57,7 +58,7 @@ type Synthdef struct {
 // Write writes a binary representation of a synthdef to an io.Writer.
 // The binary representation written by this method is
 // the data that scsynth expects at its /d_recv endpoint.
-func (self *Synthdef) Write(w io.Writer) error {
+func (def *Synthdef) Write(w io.Writer) error {
 	written, err := w.Write(bytes.NewBufferString(synthdefStart).Bytes())
 	if written != len(synthdefStart) {
 		return fmt.Errorf("Could not write synthdef")
@@ -76,66 +77,66 @@ func (self *Synthdef) Write(w io.Writer) error {
 		return err
 	}
 	// write synthdef name
-	name := newPstring(self.Name)
+	name := newPstring(def.Name)
 	err = name.Write(w)
 	if err != nil {
 		return err
 	}
 	// write number of constants
-	err = binary.Write(w, byteOrder, int32(len(self.Constants)))
+	err = binary.Write(w, byteOrder, int32(len(def.Constants)))
 	if err != nil {
 		return err
 	}
 	// write constant values
-	for _, constant := range self.Constants {
+	for _, constant := range def.Constants {
 		err = binary.Write(w, byteOrder, constant)
 		if err != nil {
 			return err
 		}
 	}
 	// write number of params
-	err = binary.Write(w, byteOrder, int32(len(self.ParamNames)))
+	err = binary.Write(w, byteOrder, int32(len(def.ParamNames)))
 	if err != nil {
 		return err
 	}
 	// write initial param values
-	for _, val := range self.InitialParamValues {
+	for _, val := range def.InitialParamValues {
 		err = binary.Write(w, byteOrder, val)
 		if err != nil {
 			return err
 		}
 	}
 	// write number of param names
-	err = binary.Write(w, byteOrder, int32(len(self.ParamNames)))
+	err = binary.Write(w, byteOrder, int32(len(def.ParamNames)))
 	if err != nil {
 		return err
 	}
 	// write param names
-	for _, p := range self.ParamNames {
+	for _, p := range def.ParamNames {
 		err = p.Write(w)
 		if err != nil {
 			return err
 		}
 	}
 	// write number of ugens
-	err = binary.Write(w, byteOrder, int32(len(self.Ugens)))
+	err = binary.Write(w, byteOrder, int32(len(def.Ugens)))
 	if err != nil {
 		return err
 	}
 	// write ugens
-	for _, u := range self.Ugens {
+	for _, u := range def.Ugens {
 		err = u.Write(w)
 		if err != nil {
 			return err
 		}
 	}
 	// write number of variants
-	err = binary.Write(w, byteOrder, int16(len(self.Variants)))
+	err = binary.Write(w, byteOrder, int16(len(def.Variants)))
 	if err != nil {
 		return err
 	}
 	// write variants
-	for _, v := range self.Variants {
+	for _, v := range def.Variants {
 		err = v.Write(w)
 		if err != nil {
 			return err
@@ -146,21 +147,21 @@ func (self *Synthdef) Write(w io.Writer) error {
 
 // WriteJSON writes a json-formatted representation of a
 // synthdef to an io.Writer
-func (self *Synthdef) WriteJSON(w io.Writer) error {
+func (def *Synthdef) WriteJSON(w io.Writer) error {
 	enc := json.NewEncoder(w)
-	return enc.Encode(self)
+	return enc.Encode(def)
 }
 
-func (self *Synthdef) WriteXML(w io.Writer) error {
+func (def *Synthdef) WriteXML(w io.Writer) error {
 	enc := xml.NewEncoder(w)
-	return enc.Encode(self)
+	return enc.Encode(def)
 }
 
 // Bytes writes a synthdef to a byte array
-func (self *Synthdef) Bytes() ([]byte, error) {
+func (def *Synthdef) Bytes() ([]byte, error) {
 	arr := make([]byte, 0)
 	buf := bytes.NewBuffer(arr)
-	err := self.Write(buf)
+	err := def.Write(buf)
 	if err != nil {
 		return arr, err
 	}
@@ -183,7 +184,7 @@ func compareBytes(a, b []byte) bool {
 }
 
 // CompareToFile compares this synthdef to another one stored on disk.
-func (self *Synthdef) CompareToFile(path string) (bool, error) {
+func (def *Synthdef) CompareToFile(path string) (bool, error) {
 	f, err := os.Open(path)
 	defer f.Close()
 	if err != nil {
@@ -194,7 +195,7 @@ func (self *Synthdef) CompareToFile(path string) (bool, error) {
 		return false, err
 	}
 	buf := bytes.NewBuffer(make([]byte, 0))
-	err = self.Write(buf)
+	err = def.Write(buf)
 	if err != nil {
 		return false, err
 	}
@@ -203,188 +204,31 @@ func (self *Synthdef) CompareToFile(path string) (bool, error) {
 }
 
 // CompareToDef compare this synthdef to another.
-func (self *Synthdef) CompareToDef(def *Synthdef) (bool, error) {
+func (def *Synthdef) CompareToDef(other *Synthdef) (bool, error) {
 	var err error
 	buf1 := bytes.NewBuffer(make([]byte, 0))
 	buf2 := bytes.NewBuffer(make([]byte, 0))
-	err = self.Write(buf1)
+	err = def.Write(buf1)
 	if err != nil {
 		return false, err
 	}
-	err = def.Write(buf2)
+	err = other.Write(buf2)
 	if err != nil {
 		return false, err
 	}
 	return compareBytes(buf1.Bytes(), buf2.Bytes()), nil
 }
 
-// WriteDiff provides a diagnostic report indicating any differences
-// between this synthdef and another.
-func (self *Synthdef) WriteDiff(w io.Writer, def *Synthdef) (bool, error) {
-	bytewise, err := self.CompareToDef(def)
-	if err != nil {
-		return false, err
-	}
-	// early out if they are bytewise identical
-	if bytewise {
-		return true, nil
-	}
-
-	same := true
-	// compare number of ugens
-	nuself := len(self.Ugens)
-	nudef := len(def.Ugens)
-	if nudef != nuself {
-		_, err = fmt.Fprintf(w, "different number of ugens: self=%d def=%d\n", nuself, nudef)
-		if err != nil {
-			return false, err
-		}
-		same = false
-	}
-
-	// compare number of constants
-	ncdef := len(def.Constants)
-	ncself := len(self.Constants)
-	if ncdef != ncself {
-		_, err = fmt.Fprintf(w, "different number of constants: self=%d def=%d\n", ncself, ncdef)
-		if err != nil {
-			return false, err
-		}
-		same = false
-	}
-
-	// compare number of InitialParamValues and ParamNames
-	nipvdef := len(def.InitialParamValues)
-	nipvself := len(self.InitialParamValues)
-	if nipvdef != nipvself {
-		_, err = fmt.Fprintf(w, "different number of initial param values: self=%d def=%d\n", nipvself, nipvdef)
-		if err != nil {
-			return false, err
-		}
-		same = false
-	}
-	npndef := len(def.ParamNames)
-	npnself := len(self.ParamNames)
-	if npndef != npnself {
-		_, err = fmt.Fprintf(w, "different number of param names self=%d def=%d\n", npnself, npndef)
-		if err != nil {
-			return false, err
-		}
-		same = false
-	}
-
-	if !same {
-		return false, nil
-	}
-	return self.writeUgensDiff(w, def, def.Ugens[nudef-1], self.Ugens[nudef-1])
-}
-
-// writeUgensDiff writes any differences in a ugen graph to an io.Writer
-// and returns a bool indicating if the ugen graphs are the same, and an error
-// for any errors that occured
-func (self *Synthdef) writeUgensDiff(w io.Writer, def *Synthdef, uself, udef *ugen) (bool, error) {
-	var err error
-	same := true
-	// Name
-	if udef.Name != uself.Name {
-		_, err = fmt.Fprintf(w, "Name is different self=%s def=%s\n", uself.Name, udef.Name)
-		if err != nil {
-			return false, err
-		}
-		same = false
-	}
-	// Rate
-	if udef.Rate != uself.Rate {
-		_, err = fmt.Fprintf(w, "Rate is different self=%d def=%d)", uself.Rate, udef.Rate)
-		if err != nil {
-			return false, err
-		}
-		same = false
-	}
-	// SpecialIndex
-	if udef.SpecialIndex != uself.SpecialIndex {
-		_, err = fmt.Fprintf(w, "SpecialIndex is different self=%d def=%d\n", udef.SpecialIndex, uself.SpecialIndex)
-		if err != nil {
-			return false, err
-		}
-		same = false
-	}
-	// number of Inputs
-	nidef := len(udef.Inputs)
-	niself := len(uself.Inputs)
-	if nidef != niself {
-		_, err = fmt.Fprintf(w, "different number of inputs self=%d def=%d\n", niself, nidef)
-		if err != nil {
-			return false, err
-		}
-		same = false
-	}
-	// number of Outputs
-	nodef := len(udef.Outputs)
-	noself := len(uself.Outputs)
-	if noself != nodef {
-		_, err = fmt.Fprintf(w, "different number of outputs self=%d def=%d\n", noself, nodef)
-		if err != nil {
-			return false, err
-		}
-		same = false
-	}
-	// ugen inputs
-	var iself input
-	for i, idef := range udef.Inputs {
-		iself = uself.Inputs[i]
-		if idef.UgenIndex == -1 {
-			if iself.UgenIndex != -1 {
-				// def's input is a constant, but self's is not
-				_, err = fmt.Fprintf(w, "input %d def is constant but self is not\n", i)
-				if err != nil {
-					return false, err
-				}
-				same = false
-				continue
-			}
-			if int32(len(def.Constants)-1) < idef.OutputIndex {
-				return false, fmt.Errorf("output index %d is too large", idef.OutputIndex)
-			}
-			if int32(len(self.Constants)-1) < iself.OutputIndex {
-				return false, fmt.Errorf("output index %d is too large", iself.OutputIndex)
-			}
-			cdef := def.Constants[idef.OutputIndex]
-			cself := self.Constants[iself.OutputIndex]
-			if cdef != cself {
-				_, err = fmt.Fprintf(w, "constants have different values self=%f def=%f\n", cself, cdef)
-				if err != nil {
-					return false, err
-				}
-				same = false
-			}
-		} else {
-			// ugen input
-			if idef.OutputIndex != iself.OutputIndex {
-				_, err = fmt.Fprintf(w, "input %d different output indices self=%d def=%d\n", iself.OutputIndex, idef.OutputIndex)
-				if err != nil {
-					return false, err
-				}
-				same = false
-			}
-			same, err = self.writeUgensDiff(w, def, def.Ugens[idef.UgenIndex], self.Ugens[iself.UgenIndex])
-			if err != nil {
-				return false, err
-			}
-		}
-	}
-	return same, nil
-}
-
 // Compare compares this synthdef byte-for-byte with
 // the synthdef sclang generates using the given string.
 // Note that using this method requires you to have sclang installed.
+// Also note that this method is _very_ slow.
 // Returns true if the synthdefs are the same, false otherwise.
-func (self *Synthdef) Compare(def string) (bool, error) {
+func (def *Synthdef) Compare(other string) (bool, error) {
 	tmp := os.TempDir()
-	scSyndef := path.Join(tmp, fmt.Sprintf("%s.scsyndef", self.Name))
+	scSyndef := path.Join(tmp, fmt.Sprintf("%s.scsyndef", def.Name))
 	const wrap = `SynthDef(\%s, %s).writeDefFile("%s"); 0.exit;`
-	contents := fmt.Sprintf(wrap, self.Name, def, tmp)
+	contents := fmt.Sprintf(wrap, def.Name, other, tmp)
 	f, err := ioutil.TempFile(tmp, "sclang_")
 	if err != nil {
 		return false, err
@@ -412,7 +256,7 @@ func (self *Synthdef) Compare(def string) (bool, error) {
 		return false, err
 	}
 	buf := bytes.NewBuffer(make([]byte, 0))
-	err = self.Write(buf)
+	err = def.Write(buf)
 	if err != nil {
 		return false, err
 	}
@@ -559,20 +403,20 @@ var constAttrs = map[string]string{"shape": "record"}
 // WriteGraph writes a dot-formatted representation of
 // a synthdef's ugen graph to an io.Writer. See
 // http://www.graphviz.org/content/dot-language.
-func (self *Synthdef) WriteGraph(w io.Writer) error {
-	graph := newGraph(self.Name)
-	for i, ugen := range self.Ugens {
+func (def *Synthdef) WriteGraph(w io.Writer) error {
+	graph := newGraph(def.Name)
+	for i, ugen := range def.Ugens {
 		ustr := fmt.Sprintf("%s_%d", ugen.Name, i)
-		graph.AddNode(self.Name, ustr, nil)
+		graph.AddNode(def.Name, ustr, nil)
 		for j := len(ugen.Inputs) - 1; j >= 0; j-- {
 			input := ugen.Inputs[j]
 			if input.UgenIndex == -1 {
-				c := self.Constants[input.OutputIndex]
+				c := def.Constants[input.OutputIndex]
 				cstr := fmt.Sprintf("%f", c)
 				graph.AddNode(ustr, cstr, constAttrs)
 				graph.AddEdge(cstr, ustr, true, nil)
 			} else {
-				subgraph := self.addsub(input.UgenIndex, self.Ugens[input.UgenIndex])
+				subgraph := def.addsub(input.UgenIndex, def.Ugens[input.UgenIndex])
 				graph.AddSubGraph(ustr, subgraph.Name, nil)
 				graph.AddEdge(subgraph.Name, ustr, true, nil)
 			}
@@ -584,18 +428,18 @@ func (self *Synthdef) WriteGraph(w io.Writer) error {
 }
 
 // addsub creates a subgraph rooted at a particular ugen
-func (self *Synthdef) addsub(idx int32, ugen *ugen) *gographviz.Graph {
+func (def *Synthdef) addsub(idx int32, ugen *ugen) *gographviz.Graph {
 	ustr := fmt.Sprintf("%s_%d", ugen.Name, idx)
 	graph := newGraph(ustr)
 	for j := len(ugen.Inputs) - 1; j >= 0; j-- {
 		input := ugen.Inputs[j]
 		if input.UgenIndex == -1 {
-			c := self.Constants[input.OutputIndex]
+			c := def.Constants[input.OutputIndex]
 			cstr := fmt.Sprintf("%f", c)
 			graph.AddNode(ustr, cstr, constAttrs)
 			graph.AddEdge(cstr, ustr, true, nil)
 		} else {
-			subgraph := self.addsub(input.UgenIndex, self.Ugens[input.UgenIndex])
+			subgraph := def.addsub(input.UgenIndex, def.Ugens[input.UgenIndex])
 			graph.AddSubGraph(ustr, subgraph.Name, nil)
 			graph.AddEdge(subgraph.Name, ustr, true, nil)
 		}
@@ -604,14 +448,14 @@ func (self *Synthdef) addsub(idx int32, ugen *ugen) *gographviz.Graph {
 }
 
 // flatten
-func (self *Synthdef) flatten(params Params) {
-	self.addParams(params)
+func (def *Synthdef) flatten(params Params) {
+	def.addParams(params)
 	// get a topologically sorted ugens list
-	ugenNodes := self.topsort(self.root)
+	ugenNodes := def.topsort(def.root)
 
 	for _, ugenNode := range ugenNodes {
 		// add ugen to synthdef
-		ugen, _, seen := self.addUgen(ugenNode)
+		ugen, _, seen := def.addUgen(ugenNode)
 		if seen {
 			continue
 		}
@@ -621,12 +465,12 @@ func (self *Synthdef) flatten(params Params) {
 		for _, input := range inputs {
 			switch v := input.(type) {
 			case Ugen:
-				_, idx, _ := self.addUgen(v)
+				_, idx, _ := def.addUgen(v)
 				for outputIndex := range v.Outputs() {
 					ugen.AppendInput(newInput(int32(idx), int32(outputIndex)))
 				}
 			case C:
-				idx := self.addConstant(v)
+				idx := def.addConstant(v)
 				ugen.AppendInput(newInput(-1, int32(idx)))
 			case *param:
 				idx := v.Index()
@@ -636,13 +480,13 @@ func (self *Synthdef) flatten(params Params) {
 				for _, min := range mins {
 					switch x := min.(type) {
 					case Ugen:
-						_, idx, _ := self.addUgen(x)
+						_, idx, _ := def.addUgen(x)
 						// will we ever need to use a different output index? [bps]
 						for outputIndex := range x.Outputs() {
 							ugen.AppendInput(newInput(int32(idx), int32(outputIndex)))
 						}
 					case C:
-						idx := self.addConstant(x)
+						idx := def.addConstant(x)
 						ugen.AppendInput(newInput(-1, int32(idx)))
 					case *param:
 						idx := x.Index()
@@ -655,9 +499,9 @@ func (self *Synthdef) flatten(params Params) {
 }
 
 // topsort performs a depth-first-search of a ugen tree
-func (self *Synthdef) topsort(root Ugen) []Ugen {
+func (def *Synthdef) topsort(root Ugen) []Ugen {
 	stack := newStack()
-	self.topsortr(root, stack, 0)
+	def.topsortr(root, stack, 0)
 	n := stack.Size()
 	ugens := make([]Ugen, n)
 	i := 0
@@ -670,20 +514,20 @@ func (self *Synthdef) topsort(root Ugen) []Ugen {
 
 // topsortr performs a depth-first-search of a ugen tree
 // starting at a given depth
-func (self *Synthdef) topsortr(root Ugen, stack *stack, depth int) {
+func (def *Synthdef) topsortr(root Ugen, stack *stack, depth int) {
 	stack.Push(root)
 	inputs := root.Inputs()
 	numInputs := len(inputs)
 	for i := numInputs - 1; i >= 0; i-- {
-		self.processUgenInput(inputs[i], stack, depth)
+		def.processUgenInput(inputs[i], stack, depth)
 	}
 }
 
 // processUgenInput processes a single ugen input
-func (self *Synthdef) processUgenInput(input Input, stack *stack, depth int) {
+func (def *Synthdef) processUgenInput(input Input, stack *stack, depth int) {
 	switch v := input.(type) {
 	case Ugen:
-		self.topsortr(v, stack, depth+1)
+		def.topsortr(v, stack, depth+1)
 		break
 	case MultiInput:
 		// multi input
@@ -692,7 +536,7 @@ func (self *Synthdef) processUgenInput(input Input, stack *stack, depth int) {
 			min := mins[j]
 			switch w := min.(type) {
 			case Ugen:
-				self.topsortr(w, stack, depth+1)
+				def.topsortr(w, stack, depth+1)
 				break
 			}
 		}
@@ -705,23 +549,23 @@ func (self *Synthdef) processUgenInput(input Input, stack *stack, depth int) {
 // (1) Add their default values to InitialParamValues
 // (2) Add their names/indices to ParamNames
 // (3) Add a Control ugen as the first ugen
-func (self *Synthdef) addParams(p Params) {
+func (def *Synthdef) addParams(p Params) {
 	paramList := p.List()
 	numParams := len(paramList)
-	self.InitialParamValues = make([]float32, numParams)
-	self.ParamNames = make([]ParamName, numParams)
+	def.InitialParamValues = make([]float32, numParams)
+	def.ParamNames = make([]ParamName, numParams)
 	for i, param := range paramList {
-		self.InitialParamValues[i] = param.InitialValue()
-		self.ParamNames[i] = ParamName{param.Name(), param.Index()}
+		def.InitialParamValues[i] = param.InitialValue()
+		def.ParamNames[i] = ParamName{param.Name(), param.Index()}
 	}
 	if numParams > 0 {
 		ctl := p.Control()
-		self.seen = append(self.seen, ctl)
+		def.seen = append(def.seen, ctl)
 		// create a list with the single Control ugen,
 		// then append any existing ugens in the synthdef
 		// to that list
 		control := []*ugen{cloneUgen(ctl)}
-		self.Ugens = append(control, self.Ugens...)
+		def.Ugens = append(control, def.Ugens...)
 	}
 }
 
@@ -729,16 +573,16 @@ func (self *Synthdef) addParams(p Params) {
 // the ugen that was added, the position in the ugens array, and
 // a flag indicating whether this is a new ugen or one that
 // has already been visited
-func (self *Synthdef) addUgen(u Ugen) (*ugen, int, bool) {
-	for i, un := range self.seen {
+func (def *Synthdef) addUgen(u Ugen) (*ugen, int, bool) {
+	for i, un := range def.seen {
 		if un == u {
-			return self.Ugens[i], i, true
+			return def.Ugens[i], i, true
 		}
 	}
-	self.seen = append(self.seen, u)
-	idx := len(self.Ugens)
+	def.seen = append(def.seen, u)
+	idx := len(def.Ugens)
 	ugen := cloneUgen(u)
-	self.Ugens = append(self.Ugens, ugen)
+	def.Ugens = append(def.Ugens, ugen)
 	return ugen, idx, false
 }
 
@@ -748,14 +592,14 @@ func (self *Synthdef) addUgen(u Ugen) (*ugen, int, bool) {
 // It ensures that constants are not added twice by returning the
 // position in the constants array of the existing constant if
 // you try to add a duplicate.
-func (self *Synthdef) addConstant(c C) int {
-	for i, f := range self.Constants {
+func (def *Synthdef) addConstant(c C) int {
+	for i, f := range def.Constants {
 		if f == float32(c) {
 			return i
 		}
 	}
-	l := len(self.Constants)
-	self.Constants = append(self.Constants, float32(c))
+	l := len(def.Constants)
+	def.Constants = append(def.Constants, float32(c))
 	return l
 }
 

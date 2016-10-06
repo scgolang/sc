2b185a20afa7c421c712e03dc452dd9633db43cc
Brian Sorahan <bsorahan@leaf.ag>
2016-10-06 13:50:45 -0500

    use float32 for Midicps argument

--- conv.go
+++ conv.go
@@ -5,6 +5,6 @@ import (
 )
 
 // Midicps converts midi note values to frequency in Hz
-func Midicps(note int) float32 {
+func Midicps(note float32) float32 {
 	return float32(440) * float32(math.Pow(2, float64(note-69)/12.0))
 }

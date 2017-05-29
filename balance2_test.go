811ffa7653e4cbdae4ddca50db97b6c225fc3e05
Brian Sorahan <bsorahan@gmail.com>
2017-05-29 12:16:33 -0500

    use var block in Balance2 test

--- balance2_test.go
+++ balance2_test.go
@@ -7,10 +7,14 @@ import (
 func TestBalance2(t *testing.T) {
 	defName := "Balance2Test"
 	def := NewSynthdef(defName, func(p Params) Ugen {
-		bus, gain := C(0), C(0.1)
-		l, r := LFSaw{Freq: C(44)}.Rate(AR), Pulse{Freq: C(33)}.Rate(AR)
-		pos := FSinOsc{Freq: C(0.5)}.Rate(KR)
-		sig := Balance2{L: l, R: r, Pos: pos, Level: gain}.Rate(AR)
+		var (
+			bus  = C(0)
+			gain = C(0.1)
+			l    = LFSaw{Freq: C(44)}.Rate(AR)
+			r    = Pulse{Freq: C(33)}.Rate(AR)
+			pos  = FSinOsc{Freq: C(0.5)}.Rate(KR)
+			sig  = Balance2{L: l, R: r, Pos: pos, Level: gain}.Rate(AR)
+		)
 		return Out{bus, sig}.Rate(AR)
 	})
 	compareAndWrite(t, defName, def)

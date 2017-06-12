1932b1a19be8a9bf1bf22a8e943f3a4ce60ed2f1
Brian Sorahan <bsorahan@gmail.com>
2017-06-12 10:48:59 -0500

    add OffsetOut

---
+++ offset_out.go
@@ -0,0 +1,18 @@
+package sc
+
+// OffsetOut outputs a signal to a bus.
+// Tthe sample offset within the bus is kept exactly; i.e. if the synth is scheduled to be started part way
+// through a control cycle, OffsetOut will maintain the correct offset by buffering the output and
+// delaying it until the exact time that the synth was scheduled for.
+type OffsetOut struct {
+	Bus      Input
+	Channels Input
+}
+
+// Rate creates a new ugen at a specific rate.
+// If rate is an unsupported value this method will cause
+// a runtime panic.
+func (out OffsetOut) Rate(rate int8) *UgenNode {
+	CheckRate(rate)
+	return NewUgenNode("OffsetOut", rate, 0, 1, out.Bus, out.Channels)
+}
